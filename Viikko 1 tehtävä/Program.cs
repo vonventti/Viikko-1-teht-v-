6324587583fd4f5fc7e0ed205b45b97bb06009c0
anders von wendt <andersvonwendt@gmail.com>
2022-06-08 18:50:28 +0300

    end of day

--- Viikko 1 tehtävä/Program.cs
+++ Viikko 1 tehtävä/Program.cs
@@ -10,95 +10,25 @@ namespace Viikko_1_tehtävä
 
         static void Main(string[] args)
         {
-
-            List<Topic> Topics = new List<Topic>();    
-
-            int id = 1;
-            // topic.ID = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter a title");
-            string title = Console.ReadLine();
-
-
-            Console.WriteLine("Enter a description");
-            string description = Console.ReadLine();
-
-
-            Console.WriteLine("Enter time to complete");
-            double estimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
-
-
-            Console.WriteLine("Enter time spent");
-            double timeSpent = Convert.ToDouble(Console.ReadLine());
-
-
-            Console.WriteLine("Enter a source");
-            string source = Console.ReadLine();
-
-            Console.WriteLine("Enter a start date dd/mm/yyyy");
-            DateTime date = DateTime.Parse(Console.ReadLine());
-            string formatted = date.ToString("dd-MM-yyyy");
-            string startDate = formatted;
-
-            Console.WriteLine("Is the topic still ongoing? (y / n)");
-            bool inProgress = Convert.ToBoolean(Console.ReadLine());
-
-            Console.WriteLine("Enter a completion date dd/mm/yyyy");
-            DateTime date1 = DateTime.Parse(Console.ReadLine());
-            string formatted1 = date1.ToString("dd-MM-yyyy");
-            string completionDate = formatted1;
-
-            Topic topic = new Topic(id, title, description, estimatedTimeToMaster, timeSpent, source, startDate, inProgress, completionDate);
-
-            // Topic topic = new Topic();
-
-            Topics.Add(topic);
-
-            string Path = @"C:\Users\Anders\textfile.txt";
-
-            using (TextWriter tw = new StreamWriter(Path))
-            {
-                foreach (Topic t in Topics)
-                {
-                    // tw.WriteLine(string.Format("Title: {0}, Description: {1}", t.Title, t.Description));
-                    tw.WriteLine(string.Format("ID: {0}  \nTitle: {1}  \nDecription: {2}  \nTime to Complete: {3}  \nTime spent: {4}  \nSource: {5}  \nStart date: {6}  \nTopic ongoin: {7}  \nCompletion date: {8}  ", 
-                        t.ID.ToString(),
-                        t.Title,
-                        t.Description, 
-                        t.EstimatedTimeToMaster.ToString(), 
-                        t.TimeSpent.ToString(), 
-                        t.Source, 
-                        t.StartDate.ToString(), 
-                        t.InProgress.ToString(), 
-                        t.CompletionDate.ToString()));
-                    Console.WriteLine(t.Title + " " + t.Description);
-                }
-            }
-
-            // 
-            //topic.ForEach(Console.WriteLine);
-
-            foreach (Topic t in Topics)
-            {
-                Console.WriteLine(t.ID + t.Title + " " + t.Description + " " + t.EstimatedTimeToMaster + " " + t.TimeSpent + " " + t.Source + " " + t.StartDate + " " + t.CompletionDate);
-            }
-           // 
+            Topic topic = Tiedot();
+            näytäLista(topic);
+            teksiTiedosto(topic);
         }
 
         public class Topic
         {
-            public Topic(int id, string title, string description, double estimatedTimeToMaster, double timeSpent, string source, string startDate, bool inProgress, string completionDate)
-            {
-                ID = id;
-                Title = title;
-                Description = description;
-                EstimatedTimeToMaster = estimatedTimeToMaster;
-                TimeSpent = timeSpent;
-                Source = source;
-                StartDate = startDate;
-                InProgress = inProgress;
-                CompletionDate = completionDate;
-            }
+            //public Topic(int id, string title, string description, double estimatedTimeToMaster, double timeSpent, string source, string startDate, bool inProgress, string completionDate)
+            //{
+            //    ID = id;
+            //    Title = title;
+            //    Description = description;
+            //    EstimatedTimeToMaster = estimatedTimeToMaster;
+            //    TimeSpent = timeSpent;
+            //    Source = source;
+            //    StartDate = startDate;
+            //    InProgress = inProgress;
+            //    CompletionDate = completionDate;
+            //}
 
             // 
             public List<Topic> Topics { get; set; }
@@ -111,14 +41,84 @@ namespace Viikko_1_tehtävä
             public string StartDate { get; set; }
             public bool InProgress { get; set; }
             public string CompletionDate { get; set; }
-
-
-            //public void parts(int ID, string title, string description, string estimatedTimeToMaster, double timeSpent, string source, DateTime startDate, bool inProgress, DateTime completionDate)
-            //{
-            //    Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}", ID, title, description, estimatedTimeToMaster, timeSpent, source, startDate, inProgress, completionDate);
-            //}
-            
-
         }
+        static Topic Tiedot()
+        {
+            Topic topic = new Topic();
+
+            Console.WriteLine("Enter an ID");
+            topic.ID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a title");
+            topic.Title = Console.ReadLine();
+
+            Console.WriteLine("Enter a description");
+            topic.Description = Console.ReadLine();
+
+            Console.WriteLine("Enter time to complete");
+            topic.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter time spent");
+            topic.TimeSpent = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a source");
+            topic.Source = Console.ReadLine();
+
+            Console.WriteLine("Enter a start date (dd/mm/yyyy)");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            string formatted = date.ToString("dd-MM-yyyy");
+            topic.StartDate = formatted;
+
+            // bool inProgress = true;
+            Console.WriteLine("Is the topic still ongoing? (y / n)");
+            string inProg = Console.ReadLine();
+            if (inProg == "n")
+            {
+                Console.WriteLine("Enter a completion date (dd/mm/yyyy)");
+                DateTime date1 = DateTime.Parse(Console.ReadLine());
+                string formatted1 = date1.ToString("dd-MM-yyyy");
+                topic.CompletionDate = formatted1;
+            } else
+            {
+                //inProgress = false;
+            }
+
+            bool inProgress = true;
+            topic.InProgress;
+            return topic;
+        }
+        static void näytäLista(Topic topic)
+        {
+            Console.WriteLine("{0}", topic.ID);
+            Console.WriteLine("{0}", topic.Title);
+            Console.WriteLine("{0}", topic.Description);
+            Console.WriteLine("{0}", topic.EstimatedTimeToMaster);
+            Console.WriteLine("{0}", topic.TimeSpent);
+            Console.WriteLine("{0}", topic.Source);
+            Console.WriteLine("{0}", topic.StartDate);
+            Console.WriteLine("{0}", topic.InProgress);
+            Console.WriteLine("{0}", topic.CompletionDate);
+           
+        }
+
+        static void teksiTiedosto(Topic topic)
+        {
+            string Path = @"C:\Users\Anders\textfile.txt";
+
+            using (TextWriter tw = new StreamWriter(Path))
+            {
+            tw.WriteLine(string.Format("ID: {0}  \nTitle: {1}  \nDecription: {2}  \nTime to Complete: {3}  \nTime spent: {4}  \nSource: {5}  \nStart date: {6}  \nTopic ongoin: {7}  \nCompletion date: {8}  ",
+                topic.ID.ToString(),
+                topic.Title,
+                topic.Description,
+                topic.EstimatedTimeToMaster.ToString(),
+                topic.TimeSpent.ToString(),
+                topic.Source,
+                topic.StartDate.ToString(),
+                topic.InProgress.ToString(),
+                topic.CompletionDate.ToString()));
+            }
+        }
+
     }
 }
