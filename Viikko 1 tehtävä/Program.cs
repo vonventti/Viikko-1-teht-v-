6355b1f9b59c4215ade3eeb85aea9ea938704d32
anders von wendt <andersvonwendt@gmail.com>
2022-06-08 14:43:31 +0300

    almost final

--- Viikko 1 tehtävä/Program.cs
+++ Viikko 1 tehtävä/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Viikko_1_tehtävä
 {
@@ -10,9 +11,9 @@ namespace Viikko_1_tehtävä
         static void Main(string[] args)
         {
 
-            List<Topic> topic = new List<Topic>();    
+            List<Topic> Topics = new List<Topic>();    
 
-            int ID = 1;
+            int id = 1;
             // topic.ID = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter a title");
@@ -41,48 +42,75 @@ namespace Viikko_1_tehtävä
 
             Console.WriteLine("Is the topic still ongoing? (y / n)");
             bool inProgress = Convert.ToBoolean(Console.ReadLine());
-            
+
             Console.WriteLine("Enter a completion date dd/mm/yyyy");
             DateTime date1 = DateTime.Parse(Console.ReadLine());
             string formatted1 = date1.ToString("dd-MM-yyyy");
             string completionDate = formatted1;
 
-            topic.Add(new Topic(ID, title, description, estimatedTimeToMaster, timeSpent, source, startDate, inProgress, completionDate));
-            topic.ForEach(Console.WriteLine);
+            Topic topic = new Topic(id, title, description, estimatedTimeToMaster, timeSpent, source, startDate, inProgress, completionDate);
 
-            foreach (Topic t in topic)
+            // Topic topic = new Topic();
+
+            Topics.Add(topic);
+
+            string Path = @"C:\Users\Anders\textfile.txt";
+
+            using (TextWriter tw = new StreamWriter(Path))
             {
-                Console.WriteLine("\n" + t.ID + " " + t.title + " " + t.description + " " + t.estimatedTimeToMaster + " " + t.timeSpent + " " + t.source + " " + t.startDate + " " + t.completionDate);
+                foreach (Topic t in Topics)
+                {
+                    // tw.WriteLine(string.Format("Title: {0}, Description: {1}", t.Title, t.Description));
+                    tw.WriteLine(string.Format("ID: {0}  \nTitle: {1}  \nDecription: {2}  \nTime to Complete: {3}  \nTime spent: {4}  \nSource: {5}  \nStart date: {6}  \nTopic ongoin: {7}  \nCompletion date: {8}  ", 
+                        t.ID.ToString(),
+                        t.Title,
+                        t.Description, 
+                        t.EstimatedTimeToMaster.ToString(), 
+                        t.TimeSpent.ToString(), 
+                        t.Source, 
+                        t.StartDate.ToString(), 
+                        t.InProgress.ToString(), 
+                        t.CompletionDate.ToString()));
+                    Console.WriteLine(t.Title + " " + t.Description);
+                }
             }
 
+            // 
+            //topic.ForEach(Console.WriteLine);
+
+            foreach (Topic t in Topics)
+            {
+                Console.WriteLine(t.ID + t.Title + " " + t.Description + " " + t.EstimatedTimeToMaster + " " + t.TimeSpent + " " + t.Source + " " + t.StartDate + " " + t.CompletionDate);
+            }
+           // 
         }
 
         public class Topic
         {
-            public Topic(int ID, string title, string description, double estimatedTimeToMaster, double timeSpent, string source, string startDate, bool inProgress, string completionDate)
+            public Topic(int id, string title, string description, double estimatedTimeToMaster, double timeSpent, string source, string startDate, bool inProgress, string completionDate)
             {
-                ID = ID;
-                title = title;
-                description = description;
-                estimatedTimeToMaster = estimatedTimeToMaster;
-                timeSpent = timeSpent;
-                source = source;
-                startDate = startDate;
-                inProgress = inProgress;
-                completionDate = completionDate;
+                ID = id;
+                Title = title;
+                Description = description;
+                EstimatedTimeToMaster = estimatedTimeToMaster;
+                TimeSpent = timeSpent;
+                Source = source;
+                StartDate = startDate;
+                InProgress = inProgress;
+                CompletionDate = completionDate;
             }
 
- 
-            public List<Topic> topic { get; set; }
+            // 
+            public List<Topic> Topics { get; set; }
             public int ID { get; set; }
-            public string title { get; set; }
-            public string description { get; set; }
-            public double estimatedTimeToMaster { get; set; }
-            public double timeSpent { get; set; }
-            public string source { get; set; }
-            public string startDate { get; set; }
-            public bool inProgress { get; set; }
-            public string completionDate { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public double EstimatedTimeToMaster { get; set; }
+            public double TimeSpent { get; set; }
+            public string Source { get; set; }
+            public string StartDate { get; set; }
+            public bool InProgress { get; set; }
+            public string CompletionDate { get; set; }
 
 
             //public void parts(int ID, string title, string description, string estimatedTimeToMaster, double timeSpent, string source, DateTime startDate, bool inProgress, DateTime completionDate)
