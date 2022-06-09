6be5b5242154a99e0167505d2a530bfa591c49d1
anders von wendt <andersvonwendt@gmail.com>
2022-06-09 17:11:55 +0300

    Final

--- Viikko 1 tehtävä/Program.cs
+++ Viikko 1 tehtävä/Program.cs
@@ -6,7 +6,7 @@ namespace Viikko_1_tehtävä
 {
     class Program
     {
-
+        // Sovellus toteutettu käyttäen luokkaa ja oliota sekä kolmea metodia (Tiedot() = kerätään tiedot käyttäjältä, näytäLista() = näytetään tiedot konsolissa, tekstiTiedosto() = syötetään tekstitiedostoon (.txt) käyttäjän syöttämät tiedot
 
         static void Main(string[] args)
         {
@@ -15,22 +15,11 @@ namespace Viikko_1_tehtävä
             teksiTiedosto(topic);
         }
 
+        // Luokka Topic sekä parametrit
+
         public class Topic
         {
-            //public Topic(int id, string title, string description, double estimatedTimeToMaster, double timeSpent, string source, string startDate, bool inProgress, string completionDate)
-            //{
-            //    ID = id;
-            //    Title = title;
-            //    Description = description;
-            //    EstimatedTimeToMaster = estimatedTimeToMaster;
-            //    TimeSpent = timeSpent;
-            //    Source = source;
-            //    StartDate = startDate;
-            //    InProgress = inProgress;
-            //    CompletionDate = completionDate;
-            //}
-
-            // 
+            // Parametrit
             public List<Topic> Topics { get; set; }
             public int ID { get; set; }
             public string Title { get; set; }
@@ -38,16 +27,31 @@ namespace Viikko_1_tehtävä
             public double EstimatedTimeToMaster { get; set; }
             public double TimeSpent { get; set; }
             public string Source { get; set; }
-            public string StartDate { get; set; }
+            public DateTime StartDate { get; set; }
             public bool InProgress { get; set; }
-            public string CompletionDate { get; set; }
+            public DateTime CompletionDate { get; set; }
         }
-        static Topic Tiedot()
+
+        // Tiedot() => Kerätään käyttäjän tiedot ja tallennetaan olion instanssiin
+        public static Topic Tiedot()
         {
+            // Olion instanssi topic
+
             Topic topic = new Topic();
 
+            int ID;
+
             Console.WriteLine("Enter an ID");
-            topic.ID = Convert.ToInt32(Console.ReadLine());
+            bool idValid = int.TryParse(Console.ReadLine(), out ID)
+                && ID > 0;
+
+            while (!idValid)
+            {
+                Console.WriteLine("Please enter a valid ID (number)");
+                idValid = int.TryParse(Console.ReadLine(), out ID)
+                    && ID > 0;
+            }
+            topic.ID = Convert.ToInt32(idValid);
 
             Console.WriteLine("Enter a title");
             topic.Title = Console.ReadLine();
@@ -55,38 +59,67 @@ namespace Viikko_1_tehtävä
             Console.WriteLine("Enter a description");
             topic.Description = Console.ReadLine();
 
+            double EstimatedTimeToMaster;
+
             Console.WriteLine("Enter time to complete");
-            topic.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
+            bool valid = double.TryParse(Console.ReadLine(), out EstimatedTimeToMaster)
+                && EstimatedTimeToMaster > 0;
+
+            while (!valid)
+            {
+                Console.WriteLine("Please enter a valid time as a double, i.e. a number or number as xx,xx?");
+                valid = double.TryParse(Console.ReadLine(), out EstimatedTimeToMaster)
+                    && EstimatedTimeToMaster > 0;
+            }
+            topic.EstimatedTimeToMaster = Convert.ToDouble(valid);
+
+            double TimeSpent;
 
             Console.WriteLine("Enter time spent");
-            topic.TimeSpent = Convert.ToDouble(Console.ReadLine());
+            bool validTime = double.TryParse(Console.ReadLine(), out TimeSpent)
+                && TimeSpent > 0;
+
+            while (!validTime)
+            {
+                Console.WriteLine("Please enter a valid time as a double, i.e. a number or number as xx,xx?");
+                validTime = double.TryParse(Console.ReadLine(), out TimeSpent)
+                    && TimeSpent > 0;
+            }
+            topic.TimeSpent = Convert.ToDouble(validTime);
 
             Console.WriteLine("Enter a source");
             topic.Source = Console.ReadLine();
 
+ 
             Console.WriteLine("Enter a start date (dd/mm/yyyy)");
             DateTime date = DateTime.Parse(Console.ReadLine());
             string formatted = date.ToString("dd-MM-yyyy");
-            topic.StartDate = formatted;
+         
+            DateTime StartDate1 = Convert.ToDateTime(formatted);
+            topic.StartDate = StartDate1;
 
-            // bool inProgress = true;
+
             Console.WriteLine("Is the topic still ongoing? (y / n)");
             string inProg = Console.ReadLine();
-            if (inProg == "n")
+            bool S = true;
+            if (inProg != null && inProg == "n")
             {
                 Console.WriteLine("Enter a completion date (dd/mm/yyyy)");
                 DateTime date1 = DateTime.Parse(Console.ReadLine());
                 string formatted1 = date1.ToString("dd-MM-yyyy");
-                topic.CompletionDate = formatted1;
-            } else
-            {
-                //inProgress = false;
+                DateTime CompletionDate1 = Convert.ToDateTime(formatted1);
+                topic.CompletionDate = CompletionDate1;
+                S = false;
+                
             }
 
-            bool inProgress = true;
-            topic.InProgress;
+            topic.InProgress = S;
+            
+
             return topic;
         }
+
+        // näytäLista() => printtaa olioon (topic) tallennetut tiedot konsoliin (Completion date sekä the topic lasted-teksti tulee myös loppuun jos InProgress on false)
         static void näytäLista(Topic topic)
         {
             Console.WriteLine("{0}", topic.ID);
@@ -97,28 +130,39 @@ namespace Viikko_1_tehtävä
             Console.WriteLine("{0}", topic.Source);
             Console.WriteLine("{0}", topic.StartDate);
             Console.WriteLine("{0}", topic.InProgress);
-            Console.WriteLine("{0}", topic.CompletionDate);
-           
+            if (topic.InProgress == false)
+            {
+                Console.WriteLine("{0}", topic.CompletionDate);
+                Console.WriteLine("The topic lasted: " + (topic.CompletionDate - topic.StartDate).TotalDays + " days");
+            }
+            
+
         }
 
+        // tekstiTiedosto() => tallentaa olion (topic) tiedot tekstidokumenttiin (textfile.txt) (Completion date sekä the topic lasted-teksti tulee myös loppuun jos InProgress on false)
         static void teksiTiedosto(Topic topic)
         {
             string Path = @"C:\Users\Anders\textfile.txt";
 
             using (TextWriter tw = new StreamWriter(Path))
             {
-            tw.WriteLine(string.Format("ID: {0}  \nTitle: {1}  \nDecription: {2}  \nTime to Complete: {3}  \nTime spent: {4}  \nSource: {5}  \nStart date: {6}  \nTopic ongoin: {7}  \nCompletion date: {8}  ",
-                topic.ID.ToString(),
-                topic.Title,
-                topic.Description,
-                topic.EstimatedTimeToMaster.ToString(),
-                topic.TimeSpent.ToString(),
-                topic.Source,
-                topic.StartDate.ToString(),
-                topic.InProgress.ToString(),
-                topic.CompletionDate.ToString()));
-            }
+                tw.WriteLine(string.Format("ID: {0}  \nTitle: {1}  \nDecription: {2}  \nTime to Complete: {3}  \nTime spent: {4}  \nSource: {5}  \nStart date: {6}  \nTopic ongoing: {7}",
+                    topic.ID.ToString(),
+                    topic.Title,
+                    topic.Description,
+                    topic.EstimatedTimeToMaster.ToString(),
+                    topic.TimeSpent.ToString(),
+                    topic.Source,
+                    topic.StartDate.ToString(),
+                    topic.InProgress.ToString()));
+                    if (topic.InProgress == false)
+                    {
+                    tw.WriteLine("Completion date: " + topic.CompletionDate);
+                    tw.WriteLine("The topic lasted: " + (topic.CompletionDate - topic.StartDate).TotalDays + " days");
+                }
+            }   
+        }
         }
 
     }
-}
+
