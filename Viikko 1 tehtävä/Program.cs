9ed46858d8ae35af94dd1371d4c699c93cd2f979
anders von wendt <andersvonwendt@gmail.com>
2022-06-19 18:03:28 +0300

    final

--- Viikko 1 tehtävä/Program.cs
+++ Viikko 1 tehtävä/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Viikko_1_tehtävä
 {
@@ -8,39 +8,85 @@ namespace Viikko_1_tehtävä
 
         static void Main(string[] args)
         {
+            List<Topic> Topics = new List<Topic>();
 
-            Topic.kerääTiedot();
             Topic.kerääTiedot();
             //Topic.kerääTiedot();
-            var lista = Topic.Topics;
-            foreach (Topic i in lista)
-            {
-                Console.WriteLine("{0}", i.ID);
-                Console.WriteLine("{0}", i.Title);
-                Console.WriteLine("{0}", i.Description);
-                Console.WriteLine("{0}", i.EstimatedTimeToMaster);
-                Console.WriteLine("{0}", i.TimeSpent);
-                Console.WriteLine("{0}", i.Source);
-                Console.WriteLine("{0}", i.StartDate);
-                Console.WriteLine("{0}", i.InProgress);
-                if (i.InProgress == false)
-                {
-                    Console.WriteLine("{0}", i.CompletionDate);
-                    Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
-                }
-            }
+            ////Topic.kerääTiedot();
+            ////var lista = Topic.Topics;
+            //Topic.teksiTiedosto();
+            Topic.tulostaTiedot();
+            //Topic.EtsiIdtaiTopic();
+            //Topic.Delete();
+            //Topic.tulostaTiedot();
+            ////Topic.tulostaTiedot();
+            //Topic.Update(lista);
+            //Topic.tulostaTiedot();
+            //Topic.readFile();
+            //Topic.EtsiIdtaiTopic(lista);
 
-            Console.WriteLine("Enter an ID");
-            var searchId = Topic.Topics.Find(num => num.ID == Convert.ToInt32(Console.ReadLine()));
-            var retrievedId = searchId.ID;
-            var retrievedTitle = searchId.Title;
-            Console.WriteLine(searchId);
-            Console.WriteLine(retrievedTitle);
+            //foreach (Topic i in lista)
+            //{
+            //    Console.WriteLine("ID: {0}", i.ID);
+            //    Console.WriteLine("Title: {0}", i.Title);
+            //    Console.WriteLine("Description: {0}", i.Description);
+            //    Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+            //    Console.WriteLine("Time spent: {0}", i.TimeSpent);
+            //    Console.WriteLine("Source: {0}", i.Source);
+            //    Console.WriteLine("Start date: {0}", i.StartDate);
+            //    Console.WriteLine("Topic in progress: {0}", i.InProgress);
+            //    if (i.InProgress == false)
+            //    {
+            //        Console.WriteLine("Completion date: {0}", i.CompletionDate);
+            //        Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
+            //    }
+            //}
+
+            //Topic.MuutaId(lista);
+
+            //Console.WriteLine("Enter an ID");
+            //var searchId = Topic.Topics.Find(num => num.ID == Convert.ToInt32(Console.ReadLine()));
+            //var retrievedId = searchId.ID;
+            //var retrievedTitle = searchId.Title;
+            //Console.WriteLine(searchId);
+            //Console.WriteLine(retrievedTitle);
             //Topic.tulostaTiedot();
 
             //Topic.teksiTiedosto();
             //Topic.EtsiId();
             //EtsiTitle();
+
+            //int typed = 1;
+
+            //var IDS = lista.Where(c => c.ID == typed);
+            //List<Topic> searchById = lista.FindAll(i => i.ID == typed).ToList();
+
+
+
+
+
+            //foreach (var myStruct in lista)
+            //{
+            //    Console.WriteLine(myStruct.ID);
+            //}
+            //Console.ReadLine();
+
+            //foreach (Topic i in searchById)
+            //{
+            //    Console.WriteLine("ID: {0}", i.ID);
+            //    Console.WriteLine("Title: {0}", i.Title);
+            //    Console.WriteLine("Description: {0}", i.Description);
+            //    Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+            //    Console.WriteLine("Time spent: {0}", i.TimeSpent);
+            //    Console.WriteLine("Source: {0}", i.Source);
+            //    Console.WriteLine("Start date: {0}", i.StartDate);
+            //    Console.WriteLine("Topic in progress: {0}", i.InProgress);
+            //    if (i.InProgress == false)
+            //    {
+            //        Console.WriteLine("Completion date: {0}", i.CompletionDate);
+            //        Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
+            //    }
+            //}
         }
     }
 
--- Viikko 1 tehtävä/Topic.cs
+++ Viikko 1 tehtävä/Topic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Viikko_1_tehtävä
 {
@@ -46,7 +47,7 @@ namespace Viikko_1_tehtävä
             this.Source = Source;
             this.StartDate = StartDate;
             this.InProgress = InProgress;
-            
+
 
         }
 
@@ -124,14 +125,15 @@ namespace Viikko_1_tehtävä
                 DateTime CompletionDate1 = Convert.ToDateTime(formatted1);
                 DateTime CompletionDate = CompletionDate1;
                 InProgress = false;
-                
+
                 Topics.Add(new Topic(ID, Title, Description, EstimatedTimeToMaster, TimeSpent, Source, StartDate, InProgress, CompletionDate));
-            } else
+            }
+            else
             {
                 Topics.Add(new Topic(ID, Title, Description, EstimatedTimeToMaster, TimeSpent, Source, StartDate, InProgress));
             }
-            
-            
+
+
 
 
 
@@ -143,17 +145,17 @@ namespace Viikko_1_tehtävä
         {
             foreach (Topic i in Topics)
             {
-                Console.WriteLine("{0}", i.ID);
-                Console.WriteLine("{0}", i.Title);
-                Console.WriteLine("{0}", i.Description);
-                Console.WriteLine("{0}", i.EstimatedTimeToMaster);
-                Console.WriteLine("{0}", i.TimeSpent);
-                Console.WriteLine("{0}", i.Source);
-                Console.WriteLine("{0}", i.StartDate);
-                Console.WriteLine("{0}", i.InProgress);
+                Console.WriteLine("ID: {0}", i.ID);
+                Console.WriteLine("Title: {0}", i.Title);
+                Console.WriteLine("Description: {0}", i.Description);
+                Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+                Console.WriteLine("Time spent: {0}", i.TimeSpent);
+                Console.WriteLine("Source: {0}", i.Source);
+                Console.WriteLine("Start date: {0}", i.StartDate);
+                Console.WriteLine("Topic in progress: {0}", i.InProgress);
                 if (i.InProgress == false)
                 {
-                    Console.WriteLine("{0}", i.CompletionDate);
+                    Console.WriteLine("Completion date: {0}", i.CompletionDate);
                     Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
                 }
             }
@@ -190,39 +192,320 @@ namespace Viikko_1_tehtävä
             }
         }
 
-        public static void EtsiId()
-        {
+        // Etsii ID:n tai Titlen perusteella
 
-            Console.WriteLine("Enter ID");
-            var idEntered = Console.ReadLine();
-            int ID;
-            while (!int.TryParse(idEntered, out ID))
+        public static void EtsiIdtaiTitle(List<Topic> lista)
+        {
+            Console.WriteLine("Syötä '1' jos haluat etsiä ID:n perusteella, tai '2' jos haluat etsiä otsikon perusteella");
+            string input = Console.ReadLine();
+            if (input == "1")
             {
-                Console.WriteLine("This is not a valid ID. Please enter a number as ID");
-                idEntered = Console.ReadLine();
+                Console.WriteLine("Enter ID to search");
+                var idEntered = Console.ReadLine();
+                int ID;
+                while (!int.TryParse(idEntered, out ID))
+                {
+                    Console.WriteLine("This is not a valid ID. Please enter a number as ID");
+                    idEntered = Console.ReadLine();
+                }
+                List<Topic> searchById = lista.FindAll(i => i.ID == ID).ToList();
+
+                foreach (Topic i in searchById)
+                {
+                    Console.WriteLine("ID: {0}", i.ID);
+                    Console.WriteLine("Title: {0}", i.Title);
+                    Console.WriteLine("Description: {0}", i.Description);
+                    Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+                    Console.WriteLine("Time spent: {0}", i.TimeSpent);
+                    Console.WriteLine("Source: {0}", i.Source);
+                    Console.WriteLine("Start date: {0}", i.StartDate);
+                    Console.WriteLine("Topic in progress: {0}", i.InProgress);
+                    if (i.InProgress == false)
+                    {
+                        Console.WriteLine("Completion date: {0}", i.CompletionDate);
+                        Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
+                    }
+                }
+
+
             }
-
-            var value = Topics.Find(item => item.ID == ID);
-
-
-            Console.WriteLine(value.ToString()); // This will return all the values of Contact. Override ToString() function for you **Bonus
-
-
-        }
-
-        public static void EtsiTitle(Topic topic)
-        {
-            Console.WriteLine("Enter a title");
-            var Title = Console.ReadLine();
-            if (topic.Title.Equals(Title))
+            else if (input == "2")
             {
-                Console.WriteLine("Otsikko löytyy");
+                Console.WriteLine("Enter a title");
+                string Title = Console.ReadLine();
+
+                List<Topic> searchByTitle = lista.FindAll(i => i.Title == Title).ToList();
+
+                foreach (Topic i in searchByTitle)
+                {
+                    Console.WriteLine("ID: {0}", i.ID);
+                    Console.WriteLine("Title: {0}", i.Title);
+                    Console.WriteLine("Description: {0}", i.Description);
+                    Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+                    Console.WriteLine("Time spent: {0}", i.TimeSpent);
+                    Console.WriteLine("Source: {0}", i.Source);
+                    Console.WriteLine("Start date: {0}", i.StartDate);
+                    Console.WriteLine("Topic in progress: {0}", i.InProgress);
+                    if (i.InProgress == false)
+                    {
+                        Console.WriteLine("Completion date: {0}", i.CompletionDate);
+                        Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
+                    }
+                }
             }
             else
             {
-                Console.WriteLine("Otsikko ei löydy");
+                Console.WriteLine("Jotain meni pieleen");
             }
         }
+
+        // Poistaa ID:n tai titlen perusteella
+
+        public static void Delete(List<Topic> lista)
+        {
+            Console.WriteLine("Syötä '1' jos haluat poistaa ID:n perusteella, tai '2' jos haluat poistaa otsikon perusteella");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("Enter an ID");
+                var idEntered = Console.ReadLine();
+                int ID;
+                while (!int.TryParse(idEntered, out ID))
+                {
+                    Console.WriteLine("This is not a valid ID. Please enter a number as ID");
+                    idEntered = Console.ReadLine();
+                }
+                lista.RemoveAll(x => x.ID == ID);
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("Enter a title");
+                string Title = Console.ReadLine();
+                lista.RemoveAll(x => x.Title == Title);
+            }
+            else
+            {
+                Console.WriteLine("Jotain meni pieleen.");
+            }
+
+        }
+
+        // Päivittää listan ID:n tai titlen perusteella
+
+        public static void Update(List<Topic> lista)
+        {
+
+            Console.WriteLine("Syötä '1' jos haluat päivittää tietoja ID:n perusteella, tai '2' jos haluat päivittää tietoja otsikon perusteella");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("Enter ID to search");
+                var idEntered = Console.ReadLine();
+                int ID;
+                while (!int.TryParse(idEntered, out ID))
+                {
+                    Console.WriteLine("This is not a valid ID. Please enter a number as ID");
+                    idEntered = Console.ReadLine();
+                }
+                var query = lista.Where(c => c.ID == ID);
+
+                var updateFields = query.First();
+
+
+
+                Console.WriteLine("Enter an ID");
+                idEntered = Console.ReadLine();
+
+                while (!int.TryParse(idEntered, out ID))
+                {
+                    Console.WriteLine("This is not a valid ID. Please enter a number as ID");
+                    idEntered = Console.ReadLine();
+                }
+                updateFields.ID = ID;
+
+
+                Console.WriteLine("Enter a title");
+                updateFields.Title = Console.ReadLine();
+
+
+                Console.WriteLine("Enter a description");
+                updateFields.Description = Console.ReadLine();
+
+
+                Console.WriteLine("Enter time to complete");
+                var timeToCompleteEntered = Console.ReadLine();
+                double EstimatedTimeToMaster;
+                while (!double.TryParse(timeToCompleteEntered, out EstimatedTimeToMaster))
+                {
+                    Console.WriteLine("This is not a valid time. Please enter a number as time to complete");
+                    timeToCompleteEntered = Console.ReadLine();
+                }
+                updateFields.EstimatedTimeToMaster = Convert.ToDouble(EstimatedTimeToMaster);
+
+
+                Console.WriteLine("Enter time spent");
+                double TimeSpent;
+                var timeSpentEntered = Console.ReadLine();
+                while (!double.TryParse(timeSpentEntered, out TimeSpent))
+                {
+                    Console.WriteLine("This is not a valid time. Please enter a number as time spent");
+                    timeSpentEntered = Console.ReadLine();
+                }
+                updateFields.TimeSpent = Convert.ToDouble(TimeSpent);
+
+                Console.WriteLine("Enter a source");
+                updateFields.Source = Console.ReadLine();
+
+
+                Console.WriteLine("Enter a start date (dd/mm/yyyy)");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                string formatted = date.ToString("dd-MM-yyyy");
+
+                DateTime StartDate1 = Convert.ToDateTime(formatted);
+                updateFields.StartDate = StartDate1;
+
+
+                Console.WriteLine("Is the topic still ongoing? (y / n)");
+                string inProg = Console.ReadLine();
+                updateFields.InProgress = true;
+                if (inProg != null && inProg == "n")
+                {
+                    Console.WriteLine("Enter a completion date (dd/mm/yyyy)");
+                    DateTime date1 = DateTime.Parse(Console.ReadLine());
+                    string formatted1 = date1.ToString("dd-MM-yyyy");
+                    DateTime CompletionDate1 = Convert.ToDateTime(formatted1);
+                    updateFields.CompletionDate = CompletionDate1;
+                    updateFields.InProgress = false;
+
+                    //lista.Add(new Topic(ID, Title, Description, EstimatedTimeToMaster, TimeSpent, Source, StartDate, InProgress, CompletionDate));
+
+                    foreach (Topic i in query)
+                    {
+                        Console.WriteLine("ID: {0}", i.ID);
+                        Console.WriteLine("Title: {0}", i.Title);
+                        Console.WriteLine("Description: {0}", i.Description);
+                        Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+                        Console.WriteLine("Time spent: {0}", i.TimeSpent);
+                        Console.WriteLine("Source: {0}", i.Source);
+                        Console.WriteLine("Start date: {0}", i.StartDate);
+                        Console.WriteLine("Topic in progress: {0}", i.InProgress);
+                        if (i.InProgress == false)
+                        {
+                            Console.WriteLine("Completion date: {0}", i.CompletionDate);
+                            Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
+                        }
+                    }
+
+                }
+
+
+
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("Enter a title");
+                string Title = Console.ReadLine();
+
+                var query = lista.Where(c => c.Title == Title);
+
+                var updateFields = query.First();
+
+
+                Console.WriteLine("Enter an ID");
+                var idEntered = Console.ReadLine();
+                int ID;
+                while (!int.TryParse(idEntered, out ID))
+                {
+                    Console.WriteLine("This is not a valid ID. Please enter a number as ID");
+                    idEntered = Console.ReadLine();
+                }
+                updateFields.ID = ID;
+
+
+                Console.WriteLine("Enter a title");
+                updateFields.Title = Console.ReadLine();
+
+
+                Console.WriteLine("Enter a description");
+                updateFields.Description = Console.ReadLine();
+
+
+                Console.WriteLine("Enter time to complete");
+                var timeToCompleteEntered = Console.ReadLine();
+                double EstimatedTimeToMaster;
+                while (!double.TryParse(timeToCompleteEntered, out EstimatedTimeToMaster))
+                {
+                    Console.WriteLine("This is not a valid time. Please enter a number as time to complete");
+                    timeToCompleteEntered = Console.ReadLine();
+                }
+                updateFields.EstimatedTimeToMaster = Convert.ToDouble(EstimatedTimeToMaster);
+
+
+                Console.WriteLine("Enter time spent");
+                double TimeSpent;
+                var timeSpentEntered = Console.ReadLine();
+                while (!double.TryParse(timeSpentEntered, out TimeSpent))
+                {
+                    Console.WriteLine("This is not a valid time. Please enter a number as time spent");
+                    timeSpentEntered = Console.ReadLine();
+                }
+                updateFields.TimeSpent = Convert.ToDouble(TimeSpent);
+
+                Console.WriteLine("Enter a source");
+                updateFields.Source = Console.ReadLine();
+
+
+                Console.WriteLine("Enter a start date (dd/mm/yyyy)");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                string formatted = date.ToString("dd-MM-yyyy");
+
+                DateTime StartDate1 = Convert.ToDateTime(formatted);
+                updateFields.StartDate = StartDate1;
+
+
+                Console.WriteLine("Is the topic still ongoing? (y / n)");
+                string inProg = Console.ReadLine();
+                updateFields.InProgress = true;
+                if (inProg != null && inProg == "n")
+                {
+                    Console.WriteLine("Enter a completion date (dd/mm/yyyy)");
+                    DateTime date1 = DateTime.Parse(Console.ReadLine());
+                    string formatted1 = date1.ToString("dd-MM-yyyy");
+                    DateTime CompletionDate1 = Convert.ToDateTime(formatted1);
+                    updateFields.CompletionDate = CompletionDate1;
+                    updateFields.InProgress = false;
+
+
+
+                    foreach (Topic i in query)
+                    {
+                        Console.WriteLine("ID: {0}", i.ID);
+                        Console.WriteLine("Title: {0}", i.Title);
+                        Console.WriteLine("Description: {0}", i.Description);
+                        Console.WriteLine("Estimated time to complete: {0}", i.EstimatedTimeToMaster);
+                        Console.WriteLine("Time spent: {0}", i.TimeSpent);
+                        Console.WriteLine("Source: {0}", i.Source);
+                        Console.WriteLine("Start date: {0}", i.StartDate);
+                        Console.WriteLine("Topic in progress: {0}", i.InProgress);
+                        if (i.InProgress == false)
+                        {
+                            Console.WriteLine("Completion date: {0}", i.CompletionDate);
+                            Console.WriteLine("The topic lasted: " + (i.CompletionDate - i.StartDate).TotalDays + " days");
+                        }
+                    }
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Didn't find");
+            }
+
+        }
+
+
     }
 
 }
